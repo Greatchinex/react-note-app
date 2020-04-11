6783e9e1342ddc645c1e17c22c4d234caa917cae
Chilaka Chinedu <greatchinex@gmail.com>
2020-04-11 19:37:57 +0100

    Basic setup for note app

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div className="container mt-3">
+      <h1>This is My note app</h1>
+    </div>
+  );
+};
+
+export default home;
