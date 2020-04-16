5ff2ff37cac75f9422c7f27ef01708a22bed282d
chillychills-hub <shittutolulope20@gmail.com>
2020-04-16 10:31:26 -0400

    User interface for notepad project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-
 import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Home/>
     </div>
   );
 }
-
 export default App;
--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import styles from "./home.module.css";
 
-const home = () => {
+
+const post = [
+    {
+      note: "Learn React",
+      id: uuidv4(),
+    },
+    {
+      note: "Read a book",
+      id: uuidv4(),
+    },
+    {
+      note: "Take a walk",
+      id: uuidv4(),
+    }
+  ];
+  
+  const Home = () => {
+    const [myPost, setMyPost] = useState (post);
+
+    console.log (myPost);
+
   return (
-    <div className="container mt-3">
-      <h1>This is My note app</h1>
-    </div>
-  );
-};
+      <div className= "container mt-3">
+        <section className= {styles.h1style}> 
+          <h1>Notes App</h1> 
+          
+        </section>
+        <section>
+          <input className={styles.inputstyle} type="text" placeholder="Enter your note here..." ></input>
+        </section>
+        <button className={styles.addbutton}> ADD </button>
 
-export default home;
+        <div className={styles.dummytext}> 
+          {myPost.map (p => {
+            return (
+              <div className="mt-3" key={p.id}> 
+                <div>
+                  <div className="card"> 
+                    <div className= "card-body">
+                      <div className="d-flex justify-content-between">
+                          {p.note}
+                          <button> Delete </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+export default Home;
